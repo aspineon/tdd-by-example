15edccfbd6fe37455806e3996a6e362f0b9a840c
Andrew Barchuk <raindevel@gmail.com>
2014-07-08 22:02:03 +0300

    Degenerate Objects

--- dollar_spec.rb
+++ dollar_spec.rb
@@ -3,7 +3,9 @@ require_relative 'dollar'
 describe Dollar do
   it 'multiplied by number' do
     five = Dollar.new 5
-    five.times 2
-    expect( five.amount ).to eq 10
+    product = five.times 2
+    expect( product.amount ).to eq 10
+    product = five.times 3
+    expect( product.amount ).to eq 15
   end
 end
