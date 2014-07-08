4e980eaa369eb0585bb26c3e71d7804b5fcf70f6
Andrew Barchuk <raindevel@gmail.com>
2014-07-08 22:04:34 +0300

    Equality for All

--- dollar_spec.rb
+++ dollar_spec.rb
@@ -8,4 +8,9 @@ describe Dollar do
     product = five.times 3
     expect( product.amount ).to eq 15
   end
+
+  it 'equals by amount' do
+    expect( Dollar.new 5 ).to eq Dollar.new 5
+    expect( Dollar.new 5 ).not_to eq Dollar.new 6
+  end
 end
